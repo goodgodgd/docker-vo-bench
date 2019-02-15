452905ab52d059c8f75b4d63d2a6e5397a8e765b
goodgodgd <goodgodgd@yonsei.ac.kr>
2019-02-15 12:40:02 +0900

    maplab runs fine

---
+++ xenial-ros/scripts/ros_kinetic_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "[Make the catkin workspace and test the catkin_make]"
+echo "[Note] Catkin workspace   >>> $WORKDIR/catkin_ws"
+WORKDIR=/work
+name_catkin_workspace=${name_catkin_workspace:="catkin_ws"}
+name_ros_version=${name_ros_version:="kinetic"}
+
+cd $WORKDIR/$name_catkin_workspace
+catkin init
+catkin config --merge-devel # Necessary for catkin_tools >= 0.4.
+catkin config --extend /opt/ros/$name_ros_version
+catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+cd src
+catkin_init_workspace
+cd ..
+catkin_make
+
+sh -c "echo \"source $WORKDIR/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+
