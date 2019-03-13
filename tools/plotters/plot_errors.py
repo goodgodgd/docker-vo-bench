d7448bae150bdfdb4142af821e490b6069af295f
goodgodgd <goodgodgd@yonsei.ac.kr>
2019-03-14 02:00:14 +0900

    update plot_errors.py to show errors of subset sequence

--- tools/plotters/plot_errors.py
+++ tools/plotters/plot_errors.py
@@ -14,26 +14,75 @@ import evaluation.eval_common as ec
 
 def plot_dataset(dataset, save_path, ate_limit, rpe_limit, rae_limit):
     matplotlib.rcParams.update({'font.size': 8})
-    fig = plt.figure(figsize=(15, 4))
     eval_path = op.join(OUTPUT_PATH, "eval_result")
 
-    ax = fig.add_subplot(131)
-    filename = op.join(eval_path, "ate", dataset, "collect_te_mean.csv")
-    errors = pd.read_csv(filename, encoding="utf-8")
-    draw_error_plot(ax, errors, "ATE", ate_limit)
+    if dataset.startswith("euroc"):
+        fignum = 0
+        figsize = (8, 6)
+        envnames = ["MH", "V1", "V2"]
+        plot_layout = 22
+    elif dataset.startswith("tum"):
+        fignum = 1
+        figsize = (8, 9)
+        envnames = ["corridor", "magistrale", "outdoors", "room", "slides"]
+        plot_layout = 32
+    else:
+        return
 
-    ax = fig.add_subplot(132)
-    filename = op.join(eval_path, "rpe", dataset, "collect_te_mean.csv")
-    errors = pd.read_csv(filename, encoding="utf-8")
-    draw_error_plot(ax, errors, "RPE", rpe_limit)
-
-    ax = fig.add_subplot(133)
-    filename = op.join(eval_path, "rpe", dataset, "collect_re_mean.csv")
-    errors = pd.read_csv(filename, encoding="utf-8")
-    draw_error_plot(ax, errors, "RAE", rae_limit)
-
-    save_name = op.join(save_path, f"{dataset}.png")
+    fig = plt.figure(num=fignum, figsize=figsize)
+    draw_ate(fig, eval_path, dataset, envnames, plot_layout, ate_limit)
+    save_name = op.join(save_path, f"{dataset}_ate.png")
     show_and_save(save_name)
+
+    draw_rpte(fig, eval_path, dataset, envnames, plot_layout, rpe_limit)
+    save_name = op.join(save_path, f"{dataset}_rpte.png")
+    show_and_save(save_name)
+
+    draw_rpre(fig, eval_path, dataset, envnames, plot_layout, rae_limit)
+    save_name = op.join(save_path, f"{dataset}_rpre.png")
+    show_and_save(save_name)
+
+
+def draw_ate(fig, eval_path, dataset, envnames, plot_layout, ate_limit):
+    filename = op.join(eval_path, "ate", dataset, "collect_te_mean.csv")
+    errors = pd.read_csv(filename, encoding="utf-8", index_col=False)
+    errors = errors.drop(columns="Unnamed: 0")
+
+    ax = fig.add_subplot(plot_layout*10 + 1)
+    draw_error_plot(ax, errors, "ATE_total", ate_limit)
+
+    for i, env in enumerate(envnames):
+        ax = fig.add_subplot(plot_layout*10 + i + 2)
+        env_errors = errors[errors["sequence"].str.contains(env)]
+        draw_error_plot(ax, env_errors, "ATE_" + env, ate_limit)
+
+
+def draw_rpte(fig, eval_path, dataset, envnames, plot_layout, ate_limit):
+    filename = op.join(eval_path, "rpe", dataset, "collect_te_mean.csv")
+    errors = pd.read_csv(filename, encoding="utf-8", index_col=False)
+    errors = errors.drop(columns="Unnamed: 0")
+
+    ax = fig.add_subplot(plot_layout*10 + 1)
+    draw_error_plot(ax, errors, "RPTE_total", ate_limit)
+
+    for i, env in enumerate(envnames):
+        ax = fig.add_subplot(plot_layout*10 + i + 2)
+        env_errors = errors[errors["sequence"].str.contains(env)]
+        draw_error_plot(ax, env_errors, "RPTE_" + env, ate_limit)
+
+
+def draw_rpre(fig, eval_path, dataset, envnames, plot_layout, ate_limit):
+    filename = op.join(eval_path, "rpe", dataset, "collect_re_mean.csv")
+    errors = pd.read_csv(filename, encoding="utf-8", index_col=False)
+    errors = errors.drop(columns="Unnamed: 0")
+
+    ax = fig.add_subplot(plot_layout*10 + 1)
+    draw_error_plot(ax, errors, "RPRE_total", ate_limit)
+
+    for i, env in enumerate(envnames):
+        ax = fig.add_subplot(plot_layout*10 + i + 2)
+        env_errors = errors[errors["sequence"].str.contains(env)]
+        draw_error_plot(ax, env_errors, "RPRE_" + env, ate_limit)
 
 
 def draw_error_plot(ax, data, ylabel, ylimit):
@@ -60,7 +109,7 @@ def show_and_save(savename):
     print("savename:", savename)
     plt.pause(0.1)
     plt.savefig(savename, dpi=100)
-    plt.waitforbuttonpress(1)
+    plt.waitforbuttonpress()
     plt.clf()
 
 
